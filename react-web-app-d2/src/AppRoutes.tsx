bf4bd4071eff993cd697839b17c552c18d3750b8
Artnalyze <artnalyze@gmail.com>
2021-12-23 11:46:48 +0700

    add pages

--- react-web-app-d2/src/AppRoutes.tsx
+++ react-web-app-d2/src/AppRoutes.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import FrontLayout from './components/layouts/FrontLayout'
+import About from './pages/about/About'
 import Home from './pages/home/Home'
+import Project from './pages/project/Project'
+import Team from './pages/team/Team'
 
 // const ARoutes = ({ element: Element, layout: Layout, ...rest }: any) => {
 //     return <Route {...rest} element={Element}></Route>
@@ -9,14 +12,10 @@ import Home from './pages/home/Home'
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route
-                path="/"
-                element={
-                    <FrontLayout>
-                        <Home />
-                    </FrontLayout>
-                }
-            />
+            <Route path="/" element={<FrontLayout><Home /></FrontLayout>} />
+            <Route path="/about" element={<FrontLayout><About /></FrontLayout>} />
+            <Route path="/teams" element={<FrontLayout><Team /></FrontLayout>} />
+            <Route path="/projects" element={<FrontLayout><Project /></FrontLayout>} />
         </Routes>
     )
 }
